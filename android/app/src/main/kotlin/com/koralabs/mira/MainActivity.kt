2c12b34407c4281a4ce203be2e67b415fcf19c34
Furkan Çalık <furkancalk325@gmail.com>
2026-01-29 18:51:02 +0300

    düzeltme

--- android/app/src/main/kotlin/com/koralabs/mira/MainActivity.kt
+++ android/app/src/main/kotlin/com/koralabs/mira/MainActivity.kt
@@ -2,14 +2,14 @@ package com.koralabs.mira
 
 import android.content.Context
 import android.content.SharedPreferences
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         // Aligns the Window to be edge-to-edge
         enableEdgeToEdge()
